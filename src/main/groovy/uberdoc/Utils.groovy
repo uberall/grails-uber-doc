e2d192aff80b80e7279d55733bbfdae84b662d90
Adam Labelle <alabelle612@gmail.com>
2020-02-10 10:41:58 +0100

    UB-22515 Simplifies shouldPublish

--- src/main/groovy/uberdoc/Utils.groovy
+++ src/main/groovy/uberdoc/Utils.groovy
@@ -1,7 +1,5 @@
 package uberdoc
 
-import java.lang.annotation.Annotation
-
 class Utils {
 
     def grailsApplication
@@ -10,19 +8,7 @@ class Utils {
         this.grailsApplication = grailsApplication
     }
 
-    boolean shouldPublish(Annotation a) {
-        if (!a) {
-            return false
-        }
-
-        if (!a.h.memberValues?.internalOnly) {
-            return true
-        }
-
-        if (grailsApplication.config.uberdoc?.publishInternalOnly) {
-            return true
-        }
-
-        return false
+    boolean shouldPublish(a) {
+        return (!a.internalOnly() || grailsApplication.config.uberdoc?.publishInternalOnly)
     }
 }
